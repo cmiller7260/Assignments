9d7c9fc8943f7eca20530b889e2f9c37768e9e1f
cmiller7260 <36648737+cmiller7260@users.noreply.github.com>
2020-04-02 17:02:48 -0400

    Add files via upload

---
+++ CSCI431-01 Final Project/ImageCompression/Compress.py
@@ -0,0 +1,19 @@
+
+"""
+CSCI431.01 Final Project
+Name : Compress
+Authors : Christopher Zealand Miller
+Email : Czm6134@g.rit.edu
+Description : this is the program part of the final - better description needed
+"""
+
+
+import sys
+
+
+def main():
+    print("random shit")
+
+
+if __name__ == "__main__":
+    main()
